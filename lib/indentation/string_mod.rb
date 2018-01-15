468c4d54f87d08c2d47db3b8ae8862a65c8fb77b
Gleb Stiblo <errandir@gmail.com>
2018-01-15 15:10:02 +0600

    change indent name

--- lib/indentation/string_mod.rb
+++ lib/indentation/string_mod.rb
@@ -6,10 +6,10 @@ class String
   #         If set to a negative value, removes that many of the specified indentation character,
   #         tabs, or spaces from the beginning of the string
   # * i_char - Character (or string) to use for indentation
-  def indent(num = nil, i_char = ' ')
+  def indentx(num = nil, i_char = ' ')
     _indent(num, i_char)
   end
-  
+
   # Indents this string
   # Arguments:
   # * num - How many of the specified indentation to use.
@@ -17,10 +17,10 @@ class String
   #         If set to a negative value, removes that many of the specified indentation character,
   #         tabs, or spaces from the beginning of the string
   # * i_char - Character (or string) to use for indentation
-  def indent!(num = nil, i_char = ' ')
+  def indentx!(num = nil, i_char = ' ')
     replace(_indent(num, i_char))
   end
-  
+
   # Split across newlines and return the fewest number of indentation characters found on each line
   def find_least_indentation(options = {:ignore_blank_lines => true, :ignore_empty_lines => true})
     # Cannot ignore empty lines unless we're also ignoring blank lines
@@ -35,26 +35,26 @@ class String
                                         end
                                       }.collect{|substr| substr.match(/^[ \t]*/).to_s.length}.min
   end
-  
+
   # Find the least indentation of all lines within this string and remove that amount (if any)
   # Can pass an optional modifier that changes the indentation amount removed
   def reset_indentation(modifier = 0)
-    indent(-find_least_indentation + modifier)
+    indentx(-find_least_indentation + modifier)
   end
-  
+
   # Replaces the current string with one that has had its indentation reset
   # Can pass an optional modifier that changes the indentation amount removed
   def reset_indentation!(modifier = 0)
-    indent!(-find_least_indentation + modifier)
+    indentx!(-find_least_indentation + modifier)
   end
-  
+
   private
   def _indent(num = nil, i_char = ' ')
     # Define number of indentations to use
     number = num
     # Default number to 2 if spaces or 1 if other
     number ||= (i_char == ' ') ? 2 : 1
-  
+
     case
     when number >= 0
       split("\n", -1).collect{|line| (i_char * number) + line}.join("\n")
@@ -70,5 +70,5 @@ class String
       end.join("\n")
     end
   end
-  
+
 end
